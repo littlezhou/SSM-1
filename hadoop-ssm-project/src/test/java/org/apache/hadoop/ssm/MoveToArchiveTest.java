0676ffaa3aa4b6b007a03f8caf3b671e17819ba9
= <liwenfeisix@163.com>
2017-02-08 16:32:56 +0800

    standard format

--- hadoop-ssm-project/src/test/java/org/apache/hadoop/ssm/MoveToArchiveTest.java
+++ hadoop-ssm-project/src/test/java/org/apache/hadoop/ssm/MoveToArchiveTest.java
@@ -1,3 +1,20 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.hadoop.ssm;
 
 import org.apache.hadoop.conf.Configuration;
@@ -21,6 +38,7 @@ public class MoveToArchiveTest {
     conf.setInt(DFSConfigKeys.DFS_BYTES_PER_CHECKSUM_KEY, DEFAULT_BLOCK_SIZE);
     conf.setStrings(DFSConfigKeys.DFS_REPLICATION_KEY,REPLICATION_KEY);
   }
+
   @Test
   public void MoveToArchive() throws Exception{
     final Configuration conf = new HdfsConfiguration();
@@ -29,7 +47,9 @@ public class MoveToArchiveTest {
   }
 
   private void testMoveToArchive(Configuration conf)throws Exception {
-    final MiniDFSCluster cluster = new MiniDFSCluster.Builder(conf).numDataNodes(3).storageTypes(new StorageType[] {StorageType.DISK,StorageType.ARCHIVE}).build();
+    final MiniDFSCluster cluster = new MiniDFSCluster.Builder(conf).
+            numDataNodes(3).storageTypes(new StorageType[] {StorageType.DISK,
+            StorageType.ARCHIVE}).build();
     try {
       cluster.waitActive();
       final DistributedFileSystem dfs = cluster.getFileSystem();
