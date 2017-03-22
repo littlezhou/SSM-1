443180f83c7c7904feed19a9a126e38d8194a054
= <liwenfeisix@163.com>
2017-03-22 14:28:12 +0800

    change throw to try

--- hadoop-ssm-project/src/main/java/org/apache/hadoop/ssm/CacheStatusReport.java
+++ hadoop-ssm-project/src/main/java/org/apache/hadoop/ssm/CacheStatusReport.java
@@ -40,9 +40,11 @@ public class CacheStatusReport {
   private CacheStatus cacheStatus;
   private Map<String, List<CacheStatus.cacheFileInfo>> reportMap;
   private Map<String, CacheStatus.nodeCacheInfo> dnCacheReportMap;
+  private DFSClient dfsClient;
 
-  public CacheStatusReport(Configuration conf) {
+  public CacheStatusReport(DFSClient client, Configuration conf) {
     this.conf = conf;
+    this.dfsClient = client;
     reportMap = new HashMap<>();
     cacheStatus = new CacheStatus();
     nodeCacheInfo = cacheStatus.new nodeCacheInfo();
@@ -52,8 +54,7 @@ public class CacheStatusReport {
   /**
    * getCacheStatusReport : Get a CacheStatus report
    */
-  public CacheStatus getCacheStatusReport() throws IOException {
-    DFSClient dfsClient = new DFSClient(conf);
+  public CacheStatus getCacheStatusReport() {
     long cacheCapacity;
     long cacheUsed;
     long cacheRemaining;
@@ -62,57 +63,60 @@ public class CacheStatusReport {
     long cacheUsedTotal = 0;
     long cacheRemaTotal = 0;
     float cacheUsedPerTotal = 0;
-    int len = dfsClient.getDatanodeStorageReport(HdfsConstants.DatanodeReportType.LIVE).length;
-    DatanodeStorageReport dnStorageReport;
-    //get info from each dataNode
-    for (int i = 0; i < len; i++) {
-      dnStorageReport = dfsClient.getDatanodeStorageReport(HdfsConstants.
-              DatanodeReportType.LIVE)[i];
-      cacheCapacity = dnStorageReport.getDatanodeInfo().getCacheCapacity();
-      cacheUsed = dnStorageReport.getDatanodeInfo().getCacheUsed();
-      cacheRemaining = dnStorageReport.getDatanodeInfo().getCacheRemaining();
-      cacheUsedPercentage = dnStorageReport.getDatanodeInfo().getCacheUsedPercent();
-      nodeCacheInfo.setCacheCapacity(cacheCapacity);
-      nodeCacheInfo.setCacheUsed(cacheUsed);
-      nodeCacheInfo.setCacheRemaining(cacheRemaining);
-      nodeCacheInfo.setCacheUsedPercentage(cacheUsedPercentage);
-      //Each host name cannot be equal
-      dnCacheReportMap.put(dnStorageReport.getDatanodeInfo().getHostName(), nodeCacheInfo);
-      //summary
-      cacheCapacityTotal += cacheCapacity;
-      cacheUsedTotal += cacheUsed;
-      cacheRemaTotal += cacheRemaining;
-      cacheUsedPerTotal += cacheUsedPercentage;
-    }
-    cacheStatus.setCacheCapacityTotal(cacheCapacityTotal);
-    cacheStatus.setCacheUsedTotal(cacheUsedTotal);
-    cacheStatus.setCacheRemainingTotal(cacheRemaTotal);
-    cacheStatus.setCacheUsedPercentageTotal(cacheUsedPerTotal);
-    //get the cacheStatusMap
-    String poolName;
-    String path;
-    Short replication;
-    CacheStatus.cacheFileInfo cacheFileInfo = cacheStatus.new cacheFileInfo();
-    CacheDirectiveInfo filter = new CacheDirectiveInfo.Builder().build();
-    RemoteIterator<CacheDirectiveEntry> remoteIterator = dfsClient.listCacheDirectives(filter);
-    while (remoteIterator.hasNext()) {
-      poolName = dfsClient.listCacheDirectives(filter).next().getInfo().getPool();
-      path = dfsClient.listCacheDirectives(filter).next().getInfo().getPath().toString();
-      replication = dfsClient.listCacheDirectives(filter).next().getInfo().getReplication();
-      if (reportMap.containsKey(poolName)) {
-        List<CacheStatus.cacheFileInfo> list = reportMap.get(poolName);
-        cacheFileInfo.setFilePath(path);
-        cacheFileInfo.setRepliNum(replication);
-        list.add(cacheFileInfo);
-        reportMap.put(poolName, list);
-      } else {
-        cacheFileInfo.setFilePath(path);
-        cacheFileInfo.setRepliNum(replication);
-        List<CacheStatus.cacheFileInfo> list = new ArrayList<>();
-        list.add(cacheFileInfo);
-        reportMap.put(poolName, list);
+    try {
+      int len = dfsClient.getDatanodeStorageReport(HdfsConstants.DatanodeReportType.LIVE).length;
+      DatanodeStorageReport dnStorageReport;
+      //get info from each dataNode
+      for (int i = 0; i < len; i++) {
+        dnStorageReport = dfsClient.getDatanodeStorageReport(HdfsConstants.DatanodeReportType.LIVE)[i];
+        cacheCapacity = dnStorageReport.getDatanodeInfo().getCacheCapacity();
+        cacheUsed = dnStorageReport.getDatanodeInfo().getCacheUsed();
+        cacheRemaining = dnStorageReport.getDatanodeInfo().getCacheRemaining();
+        cacheUsedPercentage = dnStorageReport.getDatanodeInfo().getCacheUsedPercent();
+        nodeCacheInfo.setCacheCapacity(cacheCapacity);
+        nodeCacheInfo.setCacheUsed(cacheUsed);
+        nodeCacheInfo.setCacheRemaining(cacheRemaining);
+        nodeCacheInfo.setCacheUsedPercentage(cacheUsedPercentage);
+        //Each host name cannot be equal
+        dnCacheReportMap.put(dnStorageReport.getDatanodeInfo().getHostName(), nodeCacheInfo);
+        //summary
+        cacheCapacityTotal += cacheCapacity;
+        cacheUsedTotal += cacheUsed;
+        cacheRemaTotal += cacheRemaining;
+        cacheUsedPerTotal += cacheUsedPercentage;
       }
-      remoteIterator.next();
+      cacheStatus.setCacheCapacityTotal(cacheCapacityTotal);
+      cacheStatus.setCacheUsedTotal(cacheUsedTotal);
+      cacheStatus.setCacheRemainingTotal(cacheRemaTotal);
+      cacheStatus.setCacheUsedPercentageTotal(cacheUsedPerTotal);
+      //get the cacheStatusMap
+      String poolName;
+      String path;
+      Short replication;
+      CacheStatus.cacheFileInfo cacheFileInfo = cacheStatus.new cacheFileInfo();
+      CacheDirectiveInfo filter = new CacheDirectiveInfo.Builder().build();
+      RemoteIterator<CacheDirectiveEntry> remoteIterator = dfsClient.listCacheDirectives(filter);
+      while (remoteIterator.hasNext()) {
+        poolName = dfsClient.listCacheDirectives(filter).next().getInfo().getPool();
+        path = dfsClient.listCacheDirectives(filter).next().getInfo().getPath().toString();
+        replication = dfsClient.listCacheDirectives(filter).next().getInfo().getReplication();
+        if (reportMap.containsKey(poolName)) {
+          List<CacheStatus.cacheFileInfo> list = reportMap.get(poolName);
+          cacheFileInfo.setFilePath(path);
+          cacheFileInfo.setRepliNum(replication);
+          list.add(cacheFileInfo);
+          reportMap.put(poolName, list);
+        } else {
+          cacheFileInfo.setFilePath(path);
+          cacheFileInfo.setRepliNum(replication);
+          List<CacheStatus.cacheFileInfo> list = new ArrayList<>();
+          list.add(cacheFileInfo);
+          reportMap.put(poolName, list);
+        }
+        remoteIterator.next();
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
     }
     cacheStatus.setCacheStatusMap(reportMap);
     cacheStatus.setdnCacheStatusMap(dnCacheReportMap);
