5de1fd341d425459b52f8587305e8ffa6090442e
You Yanping <yanping.you@intel.com>
2017-03-28 15:35:05 +0800

    add a default pie chart

--- hadoop-ssm-project/src/main/webapps/command/runcommand.js
+++ hadoop-ssm-project/src/main/webapps/command/runcommand.js
@@ -16,11 +16,15 @@
  * limitations under the License.
  */
 "use strict";
+//run a command of input
 function run(var1) {
     var number = var1.substring(var1.length-1);
     if (($('#cmd-'+number).val()) == "showCache") {
         var url= '/ssm/v1?op=SHOWCACHE&cmd=' + $('#cmd-'+number).val();
         $("#figure-"+number).show().siblings().hide();
+        pie(number);
+        // line(number);
+        // bar(number);
     }else {
         // alert($('#cmd-'+number).val());
         var url = '/ssm/v1?op=RUNCOMMAND&cmd=' + $('#cmd-'+number).val();
@@ -39,8 +43,9 @@ function run(var1) {
             }
         });
     }
-};
+}
 
+//create a new command input
 var i = 0;
 function newDiv(var2) {
     i = i+1;
@@ -82,6 +87,300 @@ function newDiv(var2) {
 
         '</form>');
     $("#web-console").append($div);
+}
 
-};
+//show a drowpdownlist to show different charts
+function select(var3) {
+    var number = var3.substring(var3.length-1);
+    var type = $("#select-"+number).find(':selected').val();
+    if(type === 'line') {
+        line(number);
+        $("#myChart1-"+number).show().siblings().hide();
+    }
+    if(type === 'bar') {
+        bar(number);
+        $("#myChart2-"+number).show().siblings().hide();
+    }
+    if(type === 'pie') {
+        pie(number);
+        $("#myChart3-"+number).show().siblings().hide();
+    }
+}
+
+// show a pie chart
+function pie(number) {
+    var myChart = echarts.init(document.getElementById('myChart1-'+number));
+    var option = {
+        title : {
+            text: 'Cache统计',
+            x:'center'
+        },
+        tooltip : {
+            trigger: 'item',
+            formatter: "{a} <br/>{b} : {c} ({d}%)"
+        },
+        legend: {
+            orient : 'vertical',
+            x : 'left',
+            data: (function() {
+                var arr1 = [];
+                $.ajax({
+                    type:"GET",
+                    url : 'http://localhost:9871/ssm/v1?op=SHOWCACHE',
+                    dataType:"json",
+                    async: false,
+                    success : function(result) {
+                        for (var key in result) {
+                            arr1.push(key);
+                        }
+
+                    },
+                    error : function(errorMsg) {
+                        alert("sorry, 请求数据失败");
+                        myChart.hideLoading();
+                    }
+                })
+
+                return arr1;
+            })()
+
+        },
+        toolbox: {
+            show : true,
+            feature : {
+                mark : {show: true},
+                dataView : {show: true, readOnly: false},
+//                              magicType : {
+//                                  show: true,
+//                                  type: ['pie', 'funnel'],
+//                                  option: {
+//                                      funnel: {
+//                                          x: '25%',
+//                                          width: '50%',
+//                                          funnelAlign: 'left',
+//                                          max: 1548
+//                                      }
+//                                  }
+//                              },
+                restore : {show: true},
+                saveAsImage : {show: true}
+            }
+        },
+        calculable : true,
+        series : [
+            {
+                name:'Cache使用情况',
+                type:'pie',
+                radius : '55%',
+                center: ['50%', '50%'],
+                //                  data:[{"name":"cacheCapacity","value":"3"},{"name":"cacheRemaining","value":2},{"name":"cacheUsed","value":1},{"name":"cacheUsedPercentage","value":33}]
+                data: (function() {
+                    var arr = [];
+                    $.ajax({
+                        type:"GET",
+                        url : 'http://localhost:9871/ssm/v1?op=SHOWCACHE',
+                        async : false,
+                        dataType:"json",
+                        success : function(result) {
+                            for (var key in result) {
+                                arr.push({"name":key,"value":result[key]});
+                            }
+                        },
+                        error : function(errorMsg) {
+                            alert("sorry, 请求数据失败");
+                            myChart.hideLoading();
+                        }
+                    })
+                    return arr;
+                })()
+
+            }
+
+        ]
+    };
+
+    myChart.setOption(option);
+}
+
+//show a line chart
+function line(number) {
+    var myChart = echarts.init(document.getElementById('myChart2-'+number));
+
+    //指定图表的配置项和数据
+    var option = {
+        title: {
+            //显示标题
+            text: 'Cache统计',
+            //标题显示的位置
+//                        left: 'center'
+        },
+        toolbox: {
+            show : true,
+            feature : {
+                mark : {show: true},
+                dataView : {show: true, readOnly: false},
+//                              magicType : {
+//                                  show: true,
+//                                  type: ['line'],
+//                              },
+                restore : {show: true},
+                saveAsImage : {show: true}
+            }
+        },
+        tooltip: {trigger:'axis'},
+        //在示例中我们看到图表中需要的数据为
+        //显示需要统计的分类
+        xAxis: [
+            {
+                data: (function() {
+                    var arr1 = [];
+                    $.ajax({
+                        type:"GET",
+                        url : 'http://localhost:9871/ssm/v1?op=SHOWCACHE',
+                        dataType:"json",
+                        async: false,
+                        success : function(result) {
+                            for (var key in result) {
+                                arr1.push(key);
+                            }
+
+                        },
+                        error : function(errorMsg) {
+                            alert("sorry, 请求数据失败");
+                            myChart.hideLoading();
+                        }
+                    })
+
+                    return arr1;
+                })()
+
+            }
+        ],
+
+        yAxis: {},
+        series: [{
+            name: '数量',
+            type: 'line',
+            //        data : [3,2,1,33]
+            data: (function() {
+                var arr1 = [];
+                $.ajax({
+                    type:"GET",
+                    url : 'http://localhost:9871/ssm/v1?op=SHOWCACHE',
+                    dataType:"json",
+                    async: false,
+                    success : function(result) {
+                        for (var key in result) {
+                            arr1.push(result[key]);
+                        }
+
+                    },
+                    error : function(errorMsg) {
+                        alert("sorry, 请求数据失败");
+                        myChart.hideLoading();
+                    }
+                })
+                return arr1;
+            })()
+        }]
+
+    };
+
+
+    // 使用刚指定的配置项和数据显示图表。
+    myChart.setOption(option);
+}
+// show a bar chart
+function bar (number) {
+    var myChart = echarts.init(document.getElementById('myChart3-'+number));
+
+    //指定图表的配置项和数据
+    var option = {
+        title: {
+            //显示标题
+            text: 'Cache统计',
+            //标题显示的位置
+            left: 'center'
+        },
+        toolbox: {
+            show : true,
+            feature : {
+                mark : {show: true},
+                dataView : {show: true, readOnly: false},
+//                              magicType : {
+//                                  show: true,
+//                                  type: ['bar'],
+//                              },
+                restore : {show: true},
+                saveAsImage : {show: true}
+            }
+        },
+        tooltip: {trigger:'axis'},
+        //在示例中我们看到图表中需要的数据为
+        //显示需要统计的分类
+        xAxis: [
+            {
+                data: (function() {
+                    var arr1 = [];
+                    $.ajax({
+                        type:"GET",
+                        url : 'http://localhost:9871/ssm/v1?op=SHOWCACHE',
+                        dataType:"json",
+                        async: false,
+                        success : function(result) {
+                            for (var key in result) {
+                                arr1.push(key);
+                            }
+
+                        },
+                        error : function(errorMsg) {
+                            alert("sorry, 请求数据失败");
+                            myChart.hideLoading();
+                        }
+                    })
+
+                    return arr1;
+                })()
+
+            }
+        ],
+
+        yAxis: {},
+        series: [{
+            name: '数量',
+            type: 'bar',
+            //        data : [3,2,1,33]
+            data: (function() {
+                var arr1 = [];
+                $.ajax({
+                    type:"GET",
+                    url : 'http://localhost:9871/ssm/v1?op=SHOWCACHE',
+                    dataType:"json",
+                    async: false,
+                    success : function(result) {
+                        for (var key in result) {
+                            arr1.push(result[key]);
+                        }
+
+                    },
+                    error : function(errorMsg) {
+                        alert("sorry, 请求数据失败");
+                        myChart.hideLoading();
+                    }
+                })
+                return arr1;
+            })()
+        }]
+
+    };
+
+
+    // 使用刚指定的配置项和数据显示图表。
+    myChart.setOption(option);
+}
+
+
+
+
+
 
