2698500b0fdc89ae53807c2536d867815df172b4
littlezhou <wei.zhou@intel.com>
2018-08-31 09:51:12 +0800

    Fix

--- smart-engine/src/main/java/org/smartdata/server/engine/RuleManager.java
+++ smart-engine/src/main/java/org/smartdata/server/engine/RuleManager.java
@@ -45,7 +45,6 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
-import java.util.Random;
 import java.util.concurrent.ConcurrentHashMap;
 
 /**
